178513d9024ff3c22abab2f11628b92888aada38
Wesley Estacio <wesleyestacio4@gmail.com>
2023-10-07 15:36:58 -0300

    feat: footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="text-center bg-slate-900 text-white p-2 rounded-tr-md rounded-tl-md">
+      <p>Created by Wesley Estacio</p>
+    </footer>
+  );
+}
